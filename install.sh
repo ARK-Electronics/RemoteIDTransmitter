5de57b295f3b7fd1f56f390727ac99988b7fb7d0
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-08-15 16:00:40 -0800

    small fixes

--- install.sh
+++ install.sh
@@ -8,28 +8,23 @@ export XDG_DATA_HOME="${XDG_DATA_HOME:-$DEFAULT_XDG_DATA_HOME}"
 
 sudo true
 
-pushd .
-
-echo "Installing remote-id"
+echo "Installing rid-transmitter"
 
 # clean up legacy if it exists
 sudo systemctl stop rid-transmitter &>/dev/null
 sudo systemctl disable rid-transmitter &>/dev/null
-sudo rm -rf ~/code/RemoteIDTransmitter &>/dev/null
 
 # Install dependencies
 sudo apt-get install -y astyle bluez bluez-tools libbluetooth-dev
 
 # Clone, build, and install
-git clone --recurse-submodules --depth=1 --shallow-submodules https://github.com/ARK-Electronics/RemoteIDTransmitter.git ~/code/RemoteIDTransmitter
-cd ~/code/RemoteIDTransmitter
 make install
 
 # Give the binary capabilities so it doesn't require root
 sudo setcap 'cap_net_raw,cap_net_admin+eip' /usr/local/bin/rid-transmitter
 
+# Update linker cache
 sudo ldconfig
-popd
 
 # Install the service
 sudo cp rid-transmitter.service $XDG_CONFIG_HOME/systemd/user/
