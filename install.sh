f454f8bd453bc3f906669856a5c2482e6334197a
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-08-23 13:29:49 -0800

    use install script instead of CMake

--- install.sh
+++ install.sh
@@ -5,29 +5,32 @@ DEFAULT_XDG_CONF_HOME="$HOME/.config"
 DEFAULT_XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$DEFAULT_XDG_CONF_HOME}"
 export XDG_DATA_HOME="${XDG_DATA_HOME:-$DEFAULT_XDG_DATA_HOME}"
+THIS_DIR="$(dirname "$(realpath "$BASH_SOURCE")")"
 
 sudo true
-
-echo "Installing rid-transmitter"
-
-# clean up legacy if it exists
-sudo systemctl stop rid-transmitter &>/dev/null
-sudo systemctl disable rid-transmitter &>/dev/null
 
 # Install dependencies
 sudo apt-get install -y astyle bluez bluez-tools libbluetooth-dev
 
-# Clone, build, and install
-make install
+make
+
+# Setup project directory
+sudo cp $THIS_DIR/build/rid-transmitter /usr/local/bin
+mkdir -p $XDG_DATA_HOME/rid-transmitter
+cp $THIS_DIR/config.toml $XDG_DATA_HOME/rid-transmitter/
 
 # Give the binary capabilities so it doesn't require root
 sudo setcap 'cap_net_raw,cap_net_admin+eip' /usr/local/bin/rid-transmitter
 
-# Update linker cache
-sudo ldconfig
+# Modify config file if ENV variables are set
+CONFIG_FILE="$XDG_DATA_HOME/rid-transmitter/config.toml"
 
-# Install the service
-sudo cp rid-transmitter.service $XDG_CONFIG_HOME/systemd/user/
-systemctl --user daemon-reload
-systemctl --user enable rid-transmitter.service
-systemctl --user restart rid-transmitter.service
+if [ -n "$MANUFACTURER_CODE" ]; then
+	echo "Setting manufacturer_code to: $MANUFACTURER_CODE"
+	sed -i "s/^manufacturer_code = \".*\"/manufacturer_code = \"$MANUFACTURER_CODE\"/" "$CONFIG_FILE"
+fi
+
+if [ -n "$SERIAL_NUMBER" ]; then
+	echo "Setting serial_number to: $SERIAL_NUMBER"
+	sed -i "s/^serial_number = \".*\"/serial_number = \"$SERIAL_NUMBER\"/" "$CONFIG_FILE"
+fi
